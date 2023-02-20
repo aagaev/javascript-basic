ad2641e430b5f1ea9ec98bad8973ef9842721815
aagaev <agaeana@gmail.com>
2023-02-20 23:09:45 +0400

    update function postData with Promises

--- task2/task1/js/script.js
+++ task2/task1/js/script.js
@@ -244,7 +244,7 @@ function postData (form) { //
 
         //const statusMessage = document.createElement('div');
         //statusMessage.textContent = message.loading;
-        const statusMessage = document.createElement('img');
+        const statusMessage = document.createElement('img');      
         statusMessage.src = message.loading
         statusMessage.style.cssText = `
             display: block;
@@ -252,34 +252,55 @@ function postData (form) { //
         `
         //form.append(statusMessage);  вмещает элемент прямо внутри формы, неудобно если верстка на флексах
         form.insertAdjacentElement('afterend', statusMessage); //более гибкий чем form.append, элемент будет добавляться после формы, удобно при флексах и не сплющивает(сжимает) форму
-        const request = new XMLHttpRequest();  
-        request.open('POST', 'server.php');
+        
+        // const request = new XMLHttpRequest(); заменяем на технологию FETCH
+        // request.open('POST', 'server.php');  заменяем на технологию FETCH
 
         //request.setRequestHeader('Content-type', 'multipart/form-data'); в обычном формате заголовки не нужны когда используем связку XMLHttpRequest и FormData - он установится автоматически
-        request.setRequestHeader('Content-type', 'application/json');
+        //request.setRequestHeader('Content-type', 'application/json'); в формате JSON заголовки нужны в таком виде, ныне используется FETCH, смотри выше
+       
         const formData = new FormData(form); //данные которые заполнил пользователь в form&input.value сможем получить в js и отправить на сервер в виде объекта, также есть ключ значение
 
-        const object = {};
+        const object = {}; //для того чтобы отправить JSON запрос, создаем пустой объект, в который мы затем при помощи перебора поместим данные которые есть в formData
 
         formData.forEach(function(key, value) {
             object[key] = value; // на основании данных которые есть в formData мы сформируем объект object при помощи перебора
         });
 
-        const json = JSON.stringify(object); //далее обычный объект object превращаем в формат JSON
+        const json = JSON.stringify(object); //далее обычный объект object превращаем в формат JSON, то есть трансформация new FormData формата в JSON формат 
         
-        request.send(json);
+        fetch('server.php', {
+            method: 'POST',
+            headers: { //заголовки нужны когда отправляем JSON формате
+                'Content-type': 'application/json'
+            },
+            body: json //fetch возвращает нам Промис => data, который мы затем обрабатвает в then
+        })
+        .then(data => data.text()) //модифицируем данные чтобы мы могли их получить
+        .then(data => { //data это те данные, которые нам возвращаются из Промиса, то есть которые нам вернул сервер
+            console.log(data); // request.response === data ** уточнение - с сервера возвращается data но не JSON
+            showThanksModal(message.success);
+            form.reset(); //очищаем форму
+            statusMessage.remove(); //удаляем спиннер
+        }).catch(() => {
+            showThanksModal(message.failure);
+        }).finally(() => {
+            form.reset(); //очищаем форму, выполняется всегда независимо как закончился запрос
+        })
+
+        //request.send(json);
         // request.send(formData);
 
-        request.addEventListener('load', () => {
-            if (request.status === 200) {
-                console.log(request.response);
-                showThanksModal(message.success);
-                form.reset();
-                statusMessage.remove();
-            } else {
-                showThanksModal(message.failure);;
-            }
-        })
+        // request.addEventListener('load', () => {
+        //     if (request.status === 200) {
+        //         console.log(request.response);
+        //         showThanksModal(message.success);
+        //         form.reset();
+        //         statusMessage.remove();
+        //     } else {
+        //         showThanksModal(message.failure);
+        //     }
+        // })
     })
 }
 
