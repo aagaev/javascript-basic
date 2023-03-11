1bed6440a28a049ca4c2161e15121a9b0a68ab25
aagaev <agaeana@gmail.com>
2023-03-12 01:11:03 +0400

    feat: create Calc

--- task2/task1/js/script.js
+++ task2/task1/js/script.js
@@ -572,3 +572,84 @@ dots.forEach(dot => {//перебираем и добавляем функцио
 // prev.addEventListener('click', () => {
 //     plusSlider (-1);
 // })
+
+
+//Calculator
+
+const result = document.querySelector('.calculating__result span');//куда будет записываться результат
+
+let sex = 'female', //будем использовать их в формуле функции ниже
+    height, 
+    weight, 
+    age, 
+    ratio = '1.375'; //дефолтные значения
+
+function calcTotal () {//общая ф-ция, которая будет заниматься подсчетами с формулой. Будет запускаться каждый раз когда пользователь кликнет или введет что то в input
+    if (!sex || !height || !weight || !age || !ratio) {//проверка на false
+        result.textContent = '______';
+        return;
+    }
+
+    if (sex === 'female') {//формула если выбрана female
+        result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);//округляем
+    } else {//формула если выбрана male
+        result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+ 
+    }
+};
+
+calcTotal ()//вызываем сразу, и увидем пустую строчку в '______';
+
+function getStaticInformation(parentSelector, activeClass) {//функция для получения значений с элементов (div) страницы
+    const elements = document.querySelectorAll(`${parentSelector} div`);//получаем элементы внутри родителя, что навесить делегирование на родительский элемент и отслеживать клики - НО В ДАННОМ СЛУЧАЕ ОН НЕ ПРИМЕНИМ, ПОЭТОМУ сразу навещиваем на сами элементы (так как динамически создавать и добавлять Кнопки или Элементы внутрь калькулятора не будем потому и используем навещивание сразу на элементы)  
+
+    elements.forEach(elem => {
+        elem.addEventListener('click', (e) => {
+            if (e.target.getAttribute('data-ratio')) {//условие если есть такой атрибут у объекта события, то мы устанавливаем переменной ratio ее значение и берем вытаскиваем его у e.target.getAttribute('data-ratio') - тут будет число просто
+                ratio = +e.target.getAttribute('data-ratio');
+            } else {//в этом случае работает с полом и получаем уникальный идентификатор (мужчина или женщина)
+                sex = e.target.getAttribute('id');//получаем sex - 'female' или'male'
+            }
+            console.log(ratio, sex)
+    
+            elements.forEach(elem => elem.classList.remove(activeClass));//работаем с классами активности, убираем сначала у всех
+    
+            e.target.classList.add(activeClass);//объекту события(то куда кликнули) назначаем класс Активности
+            calcTotal() //должна вызываться каждый раз, когда происходит какое то изменение на странице(ввел, удалил, кликнул - все пересчитывается)
+        })
+    })
+
+    // document.querySelector(parentSelector).addEventListener('click', (e) => { навесим делегирование на родительский элемент и отслеживать клики
+    //     if (e.target.getAttribute('data-ratio')) {
+    //         ratio = +e.target.getAttribute('data-ratio');
+  
+}
+
+getStaticInformation('#gender', 'calculating__choose-item_active')
+getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active')
+
+function getDynamicInformation(selector) {//функция для получения значений с элементов (input) страницы
+    const input = document.querySelector(selector);//получаем input с которым будем работать
+
+    input.addEventListener('input', () => {//навещиваем обработчик событий, чтоб отслеживать, что пользователь вводит каждый раз и автоматически изменять и запускать calcTotal() с новыми данными
+        switch(input.getAttribute('id')) {// в input.getAttribute('id') будет одно из значений индентификатора (height, weight и возраст)
+            case 'height': ///соответсвенно если будет рост, то значение через +input.value; запишеться в переменную height и так далее.
+                height = +input.value;
+                break;
+
+            case 'weight': //проверка на строку
+                weight = +input.value;
+                break;
+
+            case 'age': //проверка на строку
+                age = +input.value;
+                break;        
+        }
+        calcTotal() //должна вызываться каждый раз внутри input.addEventListener, когда происходит какое то изменение на странице(ввел, удалил, кликнул - все пересчитывается)
+    }); 
+    
+}  
+
+getDynamicInformation('#height')//передаем три раза сеелктор input
+getDynamicInformation('#weight')
+getDynamicInformation('#age')
